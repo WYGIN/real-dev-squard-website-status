1ee72426ecf906ff0a876eea7e628be2791974e8
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-06-04 09:22:41 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,68 @@
+const cors = require('cors');
 module.exports = {
+	middleware: [
+    		cors({
+      			origin: "*",
+    		}),
+  	],
+	async headers() { 
+		return [ 
+			{ 
+				source: '/', 
+				headers: [
+					{ 
+						key: 'Access-Control-Allow-Origin', 
+						value: '*' //'*'
+					}
+				]	
+			},
+			{ 
+				source: '/(.*)', 
+				headers: [
+					{ 
+						key: 'Access-Control-Allow-Origin', 
+						value: '*' //'*'
+					}
+				]	
+			}, 
+			{ 
+				source: '/progress/:id', 
+				headers: [
+					{ 
+						key: 'Access-Control-Allow-Origin', 
+						value: '*' //'*' 
+					}
+				]
+			},
+			{ 
+				source: '/tasks/:id', 
+				headers: [
+					{ 
+						key: 'Access-Control-Allow-Origin', 
+						value: '*' 
+					}
+				] 
+			},
+			{ 
+				source: '/tasks/:id/details', 
+				headers: [
+					{ 
+						key: 'Access-Control-Allow-Origin', 
+						value: '*' 
+					}
+				] 
+			},
+			{ 
+				source: '/tasks/:id/(.*)', 
+				headers: [
+					{ 
+						key: 'Access-Control-Allow-Origin', 
+						value: '*' 
+					}
+				] 
+			},
+		]
+	},
 	images: {
 		domains: ['raw.githubusercontent.com', 'res.cloudinary.com'],
 	},
